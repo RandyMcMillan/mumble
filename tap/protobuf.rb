3ac47d8820c1d59e43072a53f5521bc84a4361ba
randymcmillan <randymcmillan@protonmail.com>
2025-11-17 06:51:52 -0500

    tap:local brew formulas

---
+++ tap/protobuf.rb
@@ -0,0 +1,55 @@
+class Protobuf < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://protobuf.dev/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v33.1/protobuf-33.1.tar.gz"
+  sha256 "fda132cb0c86400381c0af1fe98bd0f775cb566cb247cdcc105e344e00acc30e"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "googletest" => :build
+  depends_on "abseil"
+  uses_from_macos "zlib"
+
+  def install
+    # Keep `CMAKE_CXX_STANDARD` in sync with the same variable in `abseil.rb`.
+    abseil_cxx_standard = 17
+    cmake_args = %W[
+      -DCMAKE_CXX_STANDARD=#{abseil_cxx_standard}
+      -DBUILD_SHARED_LIBS=ON
+      -Dprotobuf_BUILD_LIBPROTOC=ON
+      -Dprotobuf_BUILD_SHARED_LIBS=ON
+      -Dprotobuf_INSTALL_EXAMPLES=ON
+      -Dprotobuf_BUILD_TESTS=ON
+      -Dprotobuf_USE_EXTERNAL_GTEST=ON
+      -Dprotobuf_FORCE_FETCH_DEPENDENCIES=OFF
+      -Dprotobuf_LOCAL_DEPENDENCIES_ONLY=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "ctest", "--test-dir", "build", "--verbose"
+    system "cmake", "--install", "build"
+
+    (share/"vim/vimfiles/syntax").install "editors/proto.vim"
+    elisp.install "editors/protobuf-mode.el"
+  end
+
+  test do
+    (testpath/"test.proto").write <<~PROTO
+      syntax = "proto3";
+      package test;
+      message TestCase {
+        string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    PROTO
+    system bin/"protoc", "test.proto", "--cpp_out=."
+  end
+end
